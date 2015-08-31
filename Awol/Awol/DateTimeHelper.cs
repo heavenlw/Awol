fce4a529d59c77bc0742bde0d8601b727981473b
Awol_Law <550804648@qq.com>
2015-08-31 20:24:44 +0800

    pack all helper up as a useful dll

---
+++ Awol/Awol/DateTimeHelper.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Awol
+{
+    public static class DateTimeHelper
+    {
+        /// <summary>  
+        /// 取得某月的第一天  
+        /// </summary>  
+        /// <param name="datetime">要取得月份第一天的时间</param>  
+        /// <returns></returns>  
+        public static DateTime GetFirstDayOfMonth(int Year, int Month)
+        {
+            return Convert.ToDateTime(Year.ToString() + "-" + Month.ToString() + "-1");
+        }
+        /// <summary>  
+        /// 取得某月的最后一天  
+        /// </summary>  
+        /// <param name="datetime">要取得月份最后一天的时间</param>  
+        /// <returns></returns>  
+        public static DateTime GetLastDayOfMonth(int Year, int Month)
+        {
+            //这里的关键就是 DateTime.DaysInMonth 获得一个月中的天数
+            int Days = DateTime.DaysInMonth(Year, Month);
+            return Convert.ToDateTime(Year.ToString() + "-" + Month.ToString() + "-" + Days.ToString());
+
+        }
+        /// <summary>
+        /// Unix时间戳转换到普通时间
+        /// </summary>
+        /// <param name="unixTimeStamp">Unix时间戳</param>
+        /// <returns>世界时UTC</returns>
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+
+        /// <summary>
+        /// 普通时间转换到Unix时间戳-10位
+        /// </summary>
+        /// <param name="dateTime">世界时UTC</param>
+        /// <returns>Unix时间戳</returns>
+        public static long DateTimeToUnixTimeStamp_10(DateTime dateTime)
+        {
+            //TimeSpan span = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+            //long unixTime = (long)span.TotalSeconds;
+            //return unixTime;
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            //10位时间戳
+            var timeStamp = (long)(dateTime - startTime).TotalSeconds;
+            //13位时间戳
+            //var timeStamp = (long)(dateTime - startTime).TotalMilliseconds;
+            return timeStamp;
+
+        }
+        /// <summary>
+        /// 普通时间转换到Unix时间戳-10位
+        /// </summary>
+        /// <param name="dateTime">世界时UTC</param>
+        /// <returns>Unix时间戳</returns>
+        public static long DateTimeToUnixTimeStamp_13(DateTime dateTime)
+        {
+            //TimeSpan span = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+            //long unixTime = (long)span.TotalSeconds;
+            //return unixTime;
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            //10位时间戳
+            //var timeStamp = (long)(dateTime - startTime).TotalSeconds;
+            //13位时间戳
+            var timeStamp = (long)(dateTime - startTime).TotalMilliseconds;
+            return timeStamp;
+
+        }
+        /// <summary>
+        /// 从文本中分析时间
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static DateTime? ParseDateTime(string text)
+        {
+            DateTime? result = null;
+
+            Regex regex;
+            Match match;
+
+            regex = new Regex(@"((19|20)[0-9]{2}-\d+-\d+\s+\d+:\d+:\d+)|((19|20)[0-9]{2}-\d+-\d+\s+\d+:\d+)|((19|20)[0-9]{2}-\d+-\d+\s+\d+)|((19|20)[0-9]{2}-\d+-\d+)");
+            match = regex.Match(text);
+            if (match.Success)
+            {
+                result = DateTime.Parse(match.Captures[0].Value);
+                return result;
+            }
+
+            regex = new Regex(@"((19|20)[0-9]{2}/\d+/\d+\s+\d+:\d+:\d+)|((19|20)[0-9]{2}/\d+/\d+\s+\d+:\d+)|((19|20)[0-9]{2}/\d+/\d+\s+\d+)|((19|20)[0-9]{2}/\d+/\d+)");
+            match = regex.Match(text);
+            if (match.Success)
+            {
+                result = DateTime.Parse(match.Captures[0].Value);
+                return result;
+            }
+
+            regex = new Regex(@"((19|20)[0-9]{2}\.\d+\.\d+\s+\d+:\d+:\d+)|((19|20)[0-9]{2}\.\d+\.\d+\s+\d+:\d+)|((19|20)[0-9]{2}\.\d+\.\d+\s+\d+)|((19|20)[0-9]{2}\.\d+\.\d+)");
+            match = regex.Match(text);
+            if (match.Success)
+            {
+                result = DateTime.Parse(match.Captures[0].Value);
+                return result;
+            }
+
+            regex = new Regex(@"(\d+年\d+月\d+日\s+\d+:\d+:\d+)|(\d+年\d+月\d+日\s+\d+:\d+)|(\d+年\d+月\d+日)");
+            match = regex.Match(text);
+            if (match.Success)
+            {
+                result = DateTime.Parse(match.Captures[0].Value);
+                return result;
+            }
+
+            //格式：14-02-18 16:02:00
+            regex = new Regex(@"(\d{1,2}-\d{1,2}-\d{1,2}\s+\d{1,2}:\d{1,2}:\d{1,2})");
+            match = regex.Match(text);
+            if (match.Success)
+            {
+                result = DateTime.Parse(match.Captures[0].Value);
+                return result;
+            }
+
+            regex = new Regex(@"(?<value>((19|20)[0-9]{2}\d{2}\d{2}))");
+            match = regex.Match(text);
+            if (match.Success)
+            {
+                DateTime date;
+                if (DateTime.TryParseExact(match.Captures[0].Value, "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out date))
+                {
+                    result = date;
+                    return result;
+                }
+            }
+
+            //格式：2月14日 13:07
+            regex = new Regex(@"(\d{1,2}月\d{1,2}日\s+\d{1,2}:\d{1,2}:\d{1,2})|(\d{1,2}月\d{1,2}日\s+\d{1,2}:\d{1,2})");
+            match = regex.Match(text);
+            if (match.Success)
+            {
+                result = DateTime.Parse(match.Captures[0].Value);
+                return result;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Awol/Awol/MathHelper.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Awol
+{
+   public  static  class  MathHelper
+    { /// <summary>
+      /// 返回0.0-1.0之间的随机数，效果等同于Javascript的Math.Random()
+      /// </summary>
+      /// <returns></returns>
+        public static double MathRandom()
+        {
+            Random rnd = new Random();
+            return rnd.NextDouble();
+        }
+
+        /// <summary>
+        /// 唯一数字序列字符串，19位长
+        /// </summary>
+        /// <returns></returns>
+        public static string GuidToInt64String()
+        {
+            byte[] buffer = Guid.NewGuid().ToByteArray();
+            return BitConverter.ToInt64(buffer, 0).ToString();
+        }
+
+        /// <summary>
+        /// 唯一序列字符串，32位长
+        /// </summary>
+        /// <returns></returns>
+        public static string GuidToString()
+        {
+            return System.Guid.NewGuid().ToString("N");
+        }
+
+        /// <summary>
+        /// 组合唯一序列字符串，32位长
+        /// </summary>
+        /// <returns></returns>
+        public static string GuidCombToString(string format = "")
+        {
+            return GenerateComb().ToString(format);
+        }
+
+        /// <summary> /// Generate a new <see cref="Guid"/> using the comb algorithm. 
+        /// </summary> 
+        public static Guid GenerateComb()
+        {
+            byte[] guidArray = Guid.NewGuid().ToByteArray();
+
+            DateTime baseDate = new DateTime(1900, 1, 1);
+            DateTime now = DateTime.Now;
+
+            // Get the days and milliseconds which will be used to build    
+            //the byte string    
+            TimeSpan days = new TimeSpan(now.Ticks - baseDate.Ticks);
+            TimeSpan msecs = now.TimeOfDay;
+
+            // Convert to a byte array        
+            // Note that SQL Server is accurate to 1/300th of a    
+            // millisecond so we divide by 3.333333    
+            byte[] daysArray = BitConverter.GetBytes(days.Days);
+            byte[] msecsArray = BitConverter.GetBytes((long)
+              (msecs.TotalMilliseconds / 3.333333));
+
+            // Reverse the bytes to match SQL Servers ordering    
+            Array.Reverse(daysArray);
+            Array.Reverse(msecsArray);
+
+            // Copy the bytes into the guid    
+            Array.Copy(daysArray, daysArray.Length - 2, guidArray,
+              guidArray.Length - 6, 2);
+            Array.Copy(msecsArray, msecsArray.Length - 4, guidArray,
+              guidArray.Length - 4, 4);
+
+            return new Guid(guidArray);
+        }
+
+        /// <summary>
+        /// 中文转换成阿拉伯数字
+        /// </summary>
+        /// <param name="strChinese">中文数字，如 一百零八</param>
+        /// <returns>阿拉伯数字，如 108</returns>
+        public static int ChineseToArab(string strChinese)
+        {
+            string e = "零一二三四五六七八九";
+            string[] ew = new string[] { "十", "百", "千" };
+            string[] ej = new string[] { "万", "亿" };
+            int a = 0;
+            if (strChinese.IndexOf(ew[0]) == 0)
+                a = 10;
+            strChinese = System.Text.RegularExpressions.Regex.Replace(strChinese, e[0].ToString(), "");
+            if (System.Text.RegularExpressions.Regex.IsMatch(strChinese, "([" + e + "])$"))
+            {
+                a += e.IndexOf(System.Text.RegularExpressions.Regex.Match(strChinese, "([" + e + "])$").Value[0]);
+            }
+            if (System.Text.RegularExpressions.Regex.IsMatch(strChinese, "([" + e + "])" + ew[0]))
+            {
+                a += e.IndexOf(System.Text.RegularExpressions.Regex.Match(strChinese, "([" + e + "])" + ew[0]).Value[0]) * 10;
+            }
+            if (System.Text.RegularExpressions.Regex.IsMatch(strChinese, "([" + e + "])" + ew[1]))
+            {
+                a += e.IndexOf(System.Text.RegularExpressions.Regex.Match(strChinese, "([" + e + "])" + ew[1]).Value[0]) * 100;
+            }
+            if (System.Text.RegularExpressions.Regex.IsMatch(strChinese, "([" + e + "])" + ew[2]))
+            {
+                a += e.IndexOf(System.Text.RegularExpressions.Regex.Match(strChinese, "([" + e + "])" + ew[2]).Value[0]) * 1000;
+            }
+            return a;
+        }
+    }
+}
