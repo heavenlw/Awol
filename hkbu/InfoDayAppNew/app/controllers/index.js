9f3c21ce3b6565e5f051e88f88a2dc9998c353cd
Awol_Law <550804648@qq.com>
2015-09-20 00:31:02 +0800

    my first app in HKBU by appcelerator

---
+++ hkbu/InfoDayAppNew/app/controllers/index.js
@@ -0,0 +1,9 @@
+$.index.open();
+
+function btClick(e) {
+$.mapView.region = { latitude : 22.339468,
+longitude : 114.181879, latitudeDelta : 0.001, longitudeDelta : 0.001
+}; };
+
+function tableClick(e) {
+console.log("Table Clicked"); };
